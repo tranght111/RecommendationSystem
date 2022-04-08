a800e31d08983193b06b071eaf43bafb6693471b
TRANG HOÀNG THỊ THÙY <18120605@student.hcmus.edu.vn>
2022-04-09 00:43:31 +0700

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,123 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div id="app">
+
+    <!-- header inner -->
+         <div class="header">
+            <div class="container-fluid">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div class="logo">
+                              <a href="index.html"><img src="./assets/images/logo4.png" alt="#" /></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <div class="header_information">
+                        <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                           <span class="navbar-toggler-icon"></span>
+                           </button>
+                           <div class="collapse navbar-collapse" id="navbarsExample04">
+                              <ul class="navbar-nav mr-auto">
+                                 <li class="nav-item active">
+                                    <a class="nav-link" href="/#/Homepage"> TRANG CHỦ  </a>
+                                 </li> 
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="clients.html">CHUYÊN NGÀNH</a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="ourwork.html">GIỚI THIỆU</a>
+                                 </li>
+                              </ul>
+                              <div class="sign_btn"><a href="#">Get A Quote</a></div>
+                           </div>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+    <!-- end header inner -->
+
+<!-- Parallax Section -->
+      <div class="parallax-section parallax1">
+         <div class="grid grid-pad">
+             <div class="col-1-1">
+                  <div class="content content-header" >
+                     <h2>Hệ thống tư vấn chọn chuyên ngành cho sinh viên khoa Công nghệ Thông tin 
+                        trường Đại học Khoa học Tự nhiên</h2>
+                     <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, fuga, consectetur sequi consequuntur nisi placeat ullam maiores perferendis. Quod, nihil reiciendis saepe optio libero minus et beatae ipsam reprehenderit sequi.</p> -->
+                     <!-- <a target="_blank" class="btn btn-ghost" href="#">Find Out More</a> -->
+                 </div>
+             </div>
+         </div>
+     </div>
+<!-- End Parallax Section -->
+
+<!-- CurveUp -->
+     <svg class="curveUpColor" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
+         <path d="M0 100 C 20 0 50 0 100 100 Z"></path>
+     </svg>
+
+      <router-view/>
+
+      <!-- middle-->
+      
+      <!-- end middle-->
+     
+      <!--  footer -->
+      <footer>
+         <div class="footer">
+            <div class="container">
+               <div class="row border-top">
+                  <div class="col-md-6 padding_bottom1   ">
+                     <h3>Subscribe Now</h3>
+                     <form class="footer_form">
+                        <input class="enter" placeholder="Enter your email" type="type" name="Enter your email">
+                        <button class="submit">submit</button>
+                     </form>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="row">
+                        <div class="col-md-5 offset-md-1 padding_bottom1">
+                           <h3>Links</h3>
+                           <ul class="cont">
+                              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do </li>
+                           </ul>
+                        </div>
+                        <div class="col-md-5 offset-md-1">
+                           <h3>Contact us</h3>
+                           <ul class="cont">
+                              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do </li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="copyright">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <p>© 2019 All Rights Reserved. <a href="https://html.design/"> Free Html Templates</a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -23,4 +130,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+</style>
+
+<style>
+@import './assets/style.css';
+@import './assets/css/bootstrap.min.css';
+@import './assets/css/responsive.css';
+@import './assets/css/jquery.mCustomScrollbar.min.css';
+@import './assets/css/jquery-ui.css';
+@import './assets/css/font-awesome.min.css';
 </style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,71 @@
+<template>
+   <!-- middle-->
+      <div id="middle" class="middle">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-3 col-sm-3">
+                  <button class="button1 noselect">Tải lên bảng điểm</button>
+                  
+               </div>
+               <div class="col-md-7 col-sm-7">
+                  <div class="titlepage">
+                     <h3>Tải lên bảng điểm của bạn lấy từ portal</h3>
+                  </div>
+               </div>
+               
+            </div>
+            <!-- icon -->
+            <!-- <div class="row">
+               <div class="col-md-1">
+                  <div class="icon">
+                     <font-awesome-icon icon="fa-solid fa-down" />
+                     <i class="fa-solid fa-down" style="font-size:48px;color:red;"></i>
+                  </div>
+               </div>
+            </div> -->
+
+            <div class="row">
+               <div class="col-md-1 col-sm-1">
+                  
+               </div>
+               <div class="col-md-3 col-sm-3">
+                  <button class="button1 noselect">Làm bài đánh giá</button>
+                  
+               </div>
+               <div class="col-md-8 col-sm-8">
+                  <div class="titlepage1">
+                     <h3>Làm bài đánh giá định hướng nghề nghiệp để xác định 
+                        nghề nghiệp/hướng phát triển thích hợp với bạn</h3>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-2 col-sm-2">
+                  
+               </div>
+               <div class="col-md-3 col-sm-3">
+                  <button class="button1 noselect">Xem kết quả</button>
+                  
+               </div>
+               <div class="col-md-7 col-sm-7">
+                  <div class="titlepage2">
+                     <h3>Hiển thị chuyên ngành phù hợp với bạn</h3>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      
+      <!-- end middle-->
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+    name: 'Home',
+}
+</script>
+
+<style>
+
+</style>
