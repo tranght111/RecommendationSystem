c8d60d708f62c1b68876f33064998bf6f4dfd9de
mynuong0301 <67038749+mynuong0301@users.noreply.github.com>
2022-04-11 15:45:49 +0700

    Update Home page, Add About page

--- src/App.vue
+++ src/App.vue
@@ -10,30 +10,30 @@
                      <div class="full">
                         <div class="center-desk">
                            <div class="logo">
-                              <a href="index.html"><img src="./assets/images/logo4.png" alt="#" /></a>
+                              <a href="/#/Home"><img src="./assets/images/logo4.png" alt="#" /></a>
                            </div>
                         </div>
                      </div>
                   </div>
                   <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
                      <div class="header_information">
-                        <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                        <nav class="navigation navbar navbar-expand-md navbar-dark " data-spy="affix" data-offset-top="197">
                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                            <span class="navbar-toggler-icon"></span>
                            </button>
                            <div class="collapse navbar-collapse" id="navbarsExample04">
                               <ul class="navbar-nav mr-auto">
                                  <li class="nav-item active">
-                                    <a class="nav-link" href="/#/Homepage"> TRANG CHỦ  </a>
+                                    <a class="nav-link" href="/#/Home"> TRANG CHỦ  </a>
                                  </li> 
                                  <li class="nav-item">
-                                    <a class="nav-link" href="clients.html">CHUYÊN NGÀNH</a>
+                                    <a class="nav-link" href="/#/">CHUYÊN NGÀNH</a>
                                  </li>
                                  <li class="nav-item">
-                                    <a class="nav-link" href="ourwork.html">GIỚI THIỆU</a>
+                                    <router-link to="/About" tag="span" class="nav-link" >GIỚI THIỆU</router-link>
                                  </li>
                               </ul>
-                              <div class="sign_btn"><a href="#">Get A Quote</a></div>
+                              <!-- <div class="sign_btn"><a href="#">Get A Quote</a></div> -->
                            </div>
                         </nav>
                      </div>
@@ -43,31 +43,9 @@
          </div>
     <!-- end header inner -->
 
-<!-- Parallax Section -->
-      <div class="parallax-section parallax1">
-         <div class="grid grid-pad">
-             <div class="col-1-1">
-                  <div class="content content-header" >
-                     <h2>Hệ thống tư vấn chọn chuyên ngành cho sinh viên khoa Công nghệ Thông tin 
-                        trường Đại học Khoa học Tự nhiên</h2>
-                     <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, fuga, consectetur sequi consequuntur nisi placeat ullam maiores perferendis. Quod, nihil reiciendis saepe optio libero minus et beatae ipsam reprehenderit sequi.</p> -->
-                     <!-- <a target="_blank" class="btn btn-ghost" href="#">Find Out More</a> -->
-                 </div>
-             </div>
-         </div>
-     </div>
-<!-- End Parallax Section -->
-
-<!-- CurveUp -->
-     <svg class="curveUpColor" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
-         <path d="M0 100 C 20 0 50 0 100 100 Z"></path>
-     </svg>
 
       <router-view/>
 
-      <!-- middle-->
-      
-      <!-- end middle-->
      
       <!--  footer -->
       <footer>
